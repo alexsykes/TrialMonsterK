33d60fd97b1fe4d196c23de642bd2d25bed0a602
Alex Sykes <alex@alexsykes.net>
2022-11-07 08:02:57 +0000

    WiP - start 07_11_22

--- app/src/main/java/com/alexsykes/trialmonsterk/CourseListAdapter.kt
+++ app/src/main/java/com/alexsykes/trialmonsterk/CourseListAdapter.kt
@@ -3,16 +3,14 @@ package com.alexsykes.trialmonsterk
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.alexsykes.trialmonsterk.CourseListAdapter.CourseViewHolder
 
 class CourseListAdapter(private val courseList: List<String>) : RecyclerView.Adapter<CourseListAdapter.CourseViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
 
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.course_label, parent, false)
+            .inflate(R.layout.course_result_section, parent, false)
         return CourseViewHolder(view)
     }
 
